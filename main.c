16cbc70f2e2fe7536341acda7991aa2cfc313483
박세완 <psw4887@naver.com>
2022-03-31 12:43:25 +0900

    스켈레톤 작성

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int add(int,int);
+
+int main(int argc, char* agv[])
+{
+    int a,b,c;
+    printf("Please Enter a add");
+    scanf("%d %d",&a, &b);
+    c = add(a,b)
+    printf("add = %d\n",c);
+}
+
+int add (int a, int b)
+{
+    //TODO implementaion
+    return 0;
+}
