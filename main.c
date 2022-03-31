4d1d6e6313dab3f871a80a66f590ea0b631bef38
박세완 <psw4887@naver.com>
2022-03-31 13:21:31 +0900

    #2022-git/1 변수이름 의미있게 변경

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
-int add(int,int);
+int additional(int,int);
 
 int main(int argc, char* agv[])
 {
     int a,b,c;
-    printf("Please Enter a add");
+    printf("Please Enter a add : ");
     scanf("%d %d",&a, &b);
     c = add(a,b)
     printf("add = %d\n",c);
 }
 
-int add (int a, int b)
+int additional (int a, int b)
 {
     //TODO implementaion
     return 0;
